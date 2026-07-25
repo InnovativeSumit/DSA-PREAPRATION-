866051677b0b398eb7b52e05c1bcc4eaa2e4ae1b
Supriyo <supriyoparamanik71@gmail.com>
2026-07-25 23:10:05 +0530

    Remove Duplicates

---
+++ Leetcode26.java
@@ -0,0 +1,27 @@
+public class Leetcode26 {
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,2,3,4,4};
+
+        int k = removeDuplicates(nums);
+
+        System.out.println("k = " + k);
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
