288c1fc0e4096a742edc93dae1f22328ec7bec7d
InnovativeSumit <pal2024santosh@gmail.com>
2026-08-07 18:40:02 +0530

    DAY26

---
+++ SUMIT/JAVA/ARRAY/PREFIX SUM/PRODUCT OF AN ARRAY EXCEPT SELF/Brute.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Brute {
+    static int[] productOfArrayExceptSelf(int[] array) {
+        int n = array.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i]=1;
+            for (int j = 0; j < n; j++) {
+               if(i!=j){
+                ans[i]*= array[j];
+                   
+               }
+            }
+            
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4};
+        int[] ans = productOfArrayExceptSelf(array);
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+    }
+}
